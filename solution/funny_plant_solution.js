551bd962f7e4bc196d16eef60a4db9f0fc39c321
Matt Swann <matt@bfa.industries>
2016-02-29 11:32:11 +1100

    honk
    Honk

--- solution/funny_plant_solution.js
+++ solution/funny_plant_solution.js
@@ -16,26 +16,26 @@ console.log(calculateFeeding(150000, 250));
 
 
 ///First attempt///
-// function funnyPlant(currentPlants, neededPlants, week){
-//     var nextPlants = 0;
-//
-//     for(var i = 0; i < currentPlants.length; ++i){
-//             currentPlants[i] += 1;
-//             nextPlants += currentPlants[i];
-//     }
-//     if(nextPlants >= neededPlants)
-//             return week;
-//
-//     for(var i = 0; i < nextPlants; ++i)
-//             currentPlants.push(0);
-//
-//     return funnyPlant(currentPlants, neededPlants, week + 1);
-// }
-//
-// var numInitial = process.argv[3];
-// var numPeople = process.argv[2];
-//
-// var plantArray = [];
-// for(var i = 0; i < numInitial; ++i)
-//     plantArray.push(0);
-// console.log(funnyPlant(plantArray, numPeople, 2));
+function funnyPlant(currentPlants, neededPlants, week){
+    var nextPlants = 0;
+
+    for(var i = 0; i < currentPlants.length; ++i){
+            currentPlants[i] += 1;
+            nextPlants += currentPlants[i];
+    }
+    if(nextPlants >= neededPlants)
+            return week;
+
+    for(var i = 0; i < nextPlants; ++i)
+            currentPlants.push(0);
+
+    return funnyPlant(currentPlants, neededPlants, week + 1);
+}
+
+var numInitial = process.argv[3];
+var numPeople = process.argv[2];
+
+var plantArray = [];
+for(var i = 0; i < numInitial; ++i)
+    plantArray.push(0);
+console.log(funnyPlant(plantArray, numPeople, 2));
